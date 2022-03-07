63362856f98997b44f0772f5870cbf871b763fed
Jiahua Cui <jiahua.cui@dcmail.ca>
2022-03-06 22:32:12 -0500

    changed footer content

--- themes/cwd/footer.php
+++ themes/cwd/footer.php
@@ -38,17 +38,7 @@
 				<h5>Email:</h5>
 				<h5>info@soleperformance.com</h5>
 			</div>
-			<!-- <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'cwd' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'cwd' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'cwd' ), 'cwd', '<a href="http://jiahuacui.com">Jiahua Cui</a>' );
-				?> -->
+
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
